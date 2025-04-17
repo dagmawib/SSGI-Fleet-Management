3aea49f56c803a094cbf8e31b3b71c3f507ca844
dagmawib <dagmawibelayneh14@gmail.com>
2025-04-17 10:45:37 +0300

    all routes get protected

--- client/src/app/driver/layout.js
+++ client/src/app/driver/layout.js
@@ -1,3 +1,16 @@
+"use client";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { isAuthenticated } from "@/utils/auth";
+
 export default function DriverLayout({ children }) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      router.push("/");
+    }
+  }, [router]);
+
   return <div className="">{children}</div>;
 }
+
