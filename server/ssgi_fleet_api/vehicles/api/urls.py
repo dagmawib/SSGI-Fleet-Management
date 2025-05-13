0a1f40c2ce4969b2f8d4a79ccaa0604a4eabd80f
MichaelBirhanuGmichael <michaelbirhanualem@gmail.com>
2025-05-13 23:14:41 +0300

    Add all_drivers API endpoint to list active drivers

--- server/ssgi_fleet_api/vehicles/api/urls.py
+++ server/ssgi_fleet_api/vehicles/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import AddVehicleView, ListVehiclesView, VehicleViewSet, unassigned_drivers
+from .views import AddVehicleView, ListVehiclesView, VehicleViewSet, unassigned_drivers, all_drivers
 
 router = DefaultRouter()
 router.register(r'vehicles', VehicleViewSet, basename='vehicle')
@@ -9,5 +9,6 @@ urlpatterns = [
     path('vehicles/add/', AddVehicleView.as_view(), name='vehicle-add'),
     path('vehicles/list/', ListVehiclesView.as_view(), name='vehicle-list'),
     path('drivers/unassigned/', unassigned_drivers, name='unassigned-drivers'),
+    path('drivers/all/', all_drivers, name='all-drivers'),
     *router.urls,
 ]
